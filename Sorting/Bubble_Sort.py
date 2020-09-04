76b08ed992b472a54185a454cf09829b8f065400
Pranith Kumar <pranithgourishetty@gmail.com>
2020-09-04 13:23:15 +0530

    sorting the given array

---
+++ Sorting/Bubble_Sort.py
@@ -0,0 +1,39 @@
+### Problem Statement
+'''
+Sort the array of given integers
+'''
+
+### Problem 1
+# Time - O(n^2) | Best time - O(n)
+# Space - O(1)
+def bubbleSort(array):
+
+    for i in range(len(array)):
+        swapCheck = False
+        for j in range(0,len(array)-1-i):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+                swapCheck = True
+        if not swapCheck :
+            return array
+
+
+### Solution 2
+## Time - O(n^2)
+## Space - O(1)
+def bubbleSort2(array):
+    swapCheck = False
+    counter = 0
+    while(not swapCheck):
+        swapCheck = True
+        for i in range(len(array)-1-counter):
+            if array[i] > array[i+1]:
+                array[i],array[i+1] = array[i+1],array[i]
+                swapCheck = False
+        counter += 1
+    return array
+
+arr =  [427,-153, 240,-160,-610, -583,-27, 131]
+print(bubbleSort2(arr)) 
