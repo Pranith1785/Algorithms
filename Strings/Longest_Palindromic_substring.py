0ab6f39b5e19b566264fa986571af264dff0eaad
Pranith Kumar <pranithgourishetty@gmail.com>
2020-09-12 15:53:41 +0530

    Optimal solution

--- Strings/Longest_Palindromic_substring.py
+++ Strings/Longest_Palindromic_substring.py
@@ -27,5 +27,31 @@ def checkPalindrome(string):
         right -= 1       
     return True
 
+### Solution - 2
+## Approach: 
+## 1. from every letter moving left and right to find the palindrome string length
+## 2. Do it for both even and odd conditions
+## 3. Overwrite Longest palindrome length with previous if find any
+## Time - O(n*2n) -> O(n^2)
+## Space - O(1)
 
-print(longestPalindromicSubstring("abcdefggfedcba"))
+def longestPalindromicSubstring1(string):
+    currentLength = [0,1]
+    for i in range(1,len(string)):
+        oddlength = getLongestPalindrome(string,i-1,i+1)
+        evenLength = getLongestPalindrome(string,i-1,i)
+        longestLength = max(oddlength,evenLength,key = lambda x: x[1] - x[0])
+        currentLength = max(currentLength,longestLength,key = lambda x: x[1]-x[0])
+    return string[currentLength[0]:currentLength[1]]
+
+def getLongestPalindrome(string,leftIdx,righIdx):
+    
+    while leftIdx >=0 and righIdx < len(string) :
+        if string[leftIdx] != string[righIdx]:
+            break
+        leftIdx -= 1
+        righIdx += 1
+    return [leftIdx+1,righIdx]
+
+
+print(longestPalindromicSubstring1("abacde"))
