ab2474ae97d4a99339efe971277101190bfd6397
Pranith Kumar <pranithgourishetty@gmail.com>
2021-07-07 11:38:30 +0530

    recursive method

--- Binary Search Trees/BST_Construction.py
+++ Binary Search Trees/BST_Construction.py
@@ -112,25 +112,98 @@ class BST:
         return currentNode.value
 
                 
-                
+### Solution -2 
 
+class BST2:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    ## Time - O(log(N)) avg  | space - O(log(n))
+    ## Time - O(N) worst     | Space - O(N)
+    def insert(self,value):
+        
+        if value < self.value:
+            if self.left is None:
+                self.left = BST2(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST2(value)
+            else:
+                self.right.insert(value)
+        return self
+    
+    ## Time - O(log(N)) avg  | space - O(log(n))
+    ## Time - O(N) worst     | Space - O(N)
+    def contains(self,value):
 
+        if value < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(value)
+        elif value > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(value)
+        else:
+            return True
 
+    ## Time - O(log(N)) avg  | space - O(log(n))
+    ## Time - O(N) worst     | Space - O(N)
+    def remove(self,value,parentNode = None):
 
+        if value < self.value:
+            if self.left is not None:
+                self.left.remove(value,self)
+        elif value > self.value:
+            if self.right is not None:
+                self.right.remove(value,self)
+        else:
+            if self.left is not None and self.right is not None:
+                self.value = self.right.getMinValue()
+                self.right.remove(self.value,parentNode = self)
+            elif parentNode is None:
+                if self.left is not None:
+                    self.value = self.left.value
+                    self.right = self.left.right
+                    self.left = self.left.left
+                elif self.right is not None:
+                    self.value = self.right.value
+                    self.left = self.right.left
+                    self.right = self.right.right
+                else:
+                    pass
+            elif parentNode.left == self:
+                parentNode.left = self.left if self.left is not None else self.right
+            elif parentNode.right == self:
+                parentNode.right = self.right if self.right is not None else self.left
 
+        return self
 
+    def getMinValue(self):
+        if self.left is not None:
+            return self.left.getMinValue()
+        else:
+            return self.value
+        
 
     
 
-root = BST(10)
+root = BST2(10)
 root.insert(15)
 root.insert(12)
 root.insert(18)
 root.insert(5)
 root.insert(2)
+root.remove(15)
 
-print(root.value)
-print(root.contains(4))
+
+print(root.contains(5))
 
 
 
